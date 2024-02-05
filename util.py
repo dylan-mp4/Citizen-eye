f0da92d9c031ccaf6d068537375aa8f35a6dc02e
dylan-mp4 <72610316+dylan-mp4@users.noreply.github.com>
2024-02-05 19:32:01 +0000

    docs

--- util.py
+++ util.py
@@ -10,17 +10,27 @@ dict_char_to_int = {'O': '0',
                     'J': '3',
                     'A': '4',
                     'G': '6',
-                    'S': '5'}
+                    'S': '5'
+                    }
 
 dict_int_to_char = {'0': 'O',
                     '1': 'I',
                     '3': 'J',
                     '4': 'A',
                     '6': 'G',
-                    '5': 'S'}
-
+                    '5': 'S'
+                    }
 
 def license_complies_format(text):
+    """
+    Check if the license plate text complies with the required format.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        bool: True if the license plate complies with the format, False otherwise.
+    """
     if len(text) != 7:
         return False
 
@@ -37,6 +47,15 @@ def license_complies_format(text):
 
 
 def format_license(text):
+    """
+    Format the license plate text by converting characters using the mapping dictionaries.
+
+    Args:
+        text (str): License plate text.
+
+    Returns:
+        str: Formatted license plate text.
+    """
     license_plate_ = ''
     mapping = {0: dict_int_to_char, 1: dict_int_to_char, 4: dict_int_to_char, 5: dict_int_to_char, 6: dict_int_to_char,
                2: dict_char_to_int, 3: dict_char_to_int}
@@ -50,10 +69,20 @@ def format_license(text):
 
 
 def read_license_plate(license_plate_crop):
+    """
+    Read the license plate text from the given cropped image.
+
+    Args:
+        license_plate_crop (PIL.Image.Image): Cropped image containing the license plate.
+
+    Returns:
+        tuple: Tuple containing the formatted license plate text and its confidence score.
+    """
+
     detections = reader.readtext(license_plate_crop)
 
     for detection in detections:
-        text, score = detection
+        bbox, text, score = detection
 
         text = text.upper().replace(' ', '')
 
@@ -64,6 +93,16 @@ def read_license_plate(license_plate_crop):
 
 
 def get_car(license_plate, vehicle_track_ids):
+    """
+    Retrieve the vehicle coordinates and ID based on the license plate coordinates.
+
+    Args:
+        license_plate (tuple): Tuple containing the coordinates of the license plate (x1, y1, x2, y2, score, class_id).
+        vehicle_track_ids (list): List of vehicle track IDs and their corresponding coordinates.
+
+    Returns:
+        tuple: Tuple containing the vehicle coordinates (x1, y1, x2, y2) and ID.
+    """
     x1, y1, x2, y2, score, class_id = license_plate
 
     foundIt = False
